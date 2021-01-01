e15e1a11096b56468aa7573dec3cd82e38ef2907
Mustafa Ozdemir <mustfaozdmir@gmail.com>
2021-01-02 02:22:59 +0300

    echo is updated

--- travis.deploy.sh
+++ travis.deploy.sh
@@ -6,7 +6,8 @@ docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASS
 # Push Dockerfile to DockerHub
 docker-compose build --pull
 docker-compose push
-
+# Installing AWS CLI
+# Installing AWS CLI
 # Installing AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip > /dev/null
